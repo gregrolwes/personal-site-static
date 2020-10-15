9b0e318055c42e4e2d6fc6dca66706ca164959a6
Greg <gregory.rolwes@slu.edu>
2020-10-15 13:39:58 -0500

    shortened animation

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,23 +1,21 @@
 var speed = 60; /* The speed/duration of the effect in milliseconds */
 
-var name = "greg rowels rowles rolwes.";
+var name = "greg rowles rolwes.";
 
 i=0;
 function confusedTypeWriter() {
     if (i < 11) {
         document.getElementById("name").innerHTML += name.charAt(i);
-    } else if ((i >= 11  && i < 18) || (i >= 25 && i  < 31)) {
+    } else if (i >= 11  && i < 18) {
         var current = document.getElementById("name").innerHTML;
         document.getElementById("name").innerHTML = current.slice(0, current.length - 1);
-    } else if (i < 29) {
-        document.getElementById("name").innerHTML += name.charAt(i-7);
     } else {
-        document.getElementById("name").innerHTML += name.charAt(i-12);
+        document.getElementById("name").innerHTML += name.charAt(i-7);
     }
     i++;
     if (i < name.length+12) {
-        var long_pause = [11,18,21,25,31,33];
-        var short_pause = [19, 32];
+        var long_pause = [11,18,21,25];
+        var short_pause = [19];
 
         if (long_pause.includes(i)) {
             setTimeout(confusedTypeWriter, speed+750);
