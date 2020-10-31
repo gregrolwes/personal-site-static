a84bbc7e69286327d7ce72508aa617a1dbbf013b
Greg <gregory.rolwes@slu.edu>
2020-10-31 11:58:35 -0500

    Added 'click me' button and shortened animation

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,28 +1,26 @@
 var speed = 70; /* The speed/duration of the effect in milliseconds */
 
-var name = "greg rowles rolwes.";
+var name = "greg rowleslwes.";
 
 i=0;
 function confusedTypeWriter() {
     if (i < 11) {
         document.getElementById("name").innerHTML += name.charAt(i);
-    } else if (i >= 11  && i < 18) {
+    } else if (i >= 11  && i < 15) {
         var current = document.getElementById("name").innerHTML;
         document.getElementById("name").innerHTML = current.slice(0, current.length - 1);
     } else {
-        document.getElementById("name").innerHTML += name.charAt(i-7);
+        document.getElementById("name").innerHTML += name.charAt(i-4);
     }
     i++;
     if (i < name.length+12) {
-        var long_pause = [11,18,21,25];
-        var short_pause = [19];
+        var long_pause = [11,15,19];
+        var short_pause = [];
 
         if (long_pause.includes(i)) {
-            setTimeout(confusedTypeWriter, speed+750);
+            setTimeout(confusedTypeWriter, speed+500);
         } else if (short_pause.includes(i)) {
             setTimeout(confusedTypeWriter, speed+50);
-        } else if (i == name.length+11) {
-            setTimeout(confusedTypeWriter, speed+1000);
         } else {
             setTimeout(confusedTypeWriter, speed);
         }
